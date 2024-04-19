3fe27261f8c84ae2957574331e3f762e7d83c720
ggulez <giogulez@gmail.com>
2024-04-19 12:01:39 -0400

    minor cosmetic

--- luminasyncflutter/lib/buildProfileTab.dart
+++ luminasyncflutter/lib/buildProfileTab.dart
@@ -18,30 +18,60 @@ class ProfileTab extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Welcome $displayName'),
-          const SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: () async {
-              SharedPreferences prefs = await SharedPreferences.getInstance();
-              String? userDetails = prefs.getString('user');
-              if (userDetails != null) {
-                Map<String, dynamic> user = jsonDecode(userDetails);
-                String userId = user['userid'].toString();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => DeviceManagerPage(userId: userId)),
-                );
-              } else {
-                // Handle error or prompt login
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text(
-                        "User not logged in. Please log in to manage devices.")));
-              }
-            },
-            child: const Text('Manage Devices'),
+          Text(
+            'Welcome $displayName',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Expanded(
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  String? userDetails = prefs.getString('user');
+                  if (userDetails != null) {
+                    Map<String, dynamic> user = jsonDecode(userDetails);
+                    String userId = user['userid'].toString();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              DeviceManagerPage(userId: userId)),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                        "No device associated with the user",
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                    ));
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(
+                    vertical: 12.0,
+                    horizontal: 24.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                child: Text(
+                  'Manage Devices',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
